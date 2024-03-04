01856a4203eb0e6a409784944a0eb1d6a3009e2b
Mingcan <mt229a@togpu1.mvls.gla.ac.uk>
2024-03-04 11:48:46 +0000

    new users can select built-in data without uploading expression fil

---
+++ accounts/views.py
@@ -0,0 +1,30 @@
+from django.shortcuts import render,redirect
+from django import forms
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
+from django.core.exceptions import ValidationError
+from django.urls import reverse_lazy
+from django.views.generic import CreateView
+# Create your views here.
+
+def validate_username(value):
+    if '_' in value:
+        raise ValidationError('Usernames cannot contain underscores.')
+    
+class SignUpForm(UserCreationForm):
+    username=forms.CharField(max_length=30,validators=[validate_username],help_text="Required. not allowed '_'")
+    email=forms.EmailField(max_length=100,help_text='Required. Enter a valid email address.')
+
+    class Meta:
+        model=User
+        fields=('username','email','password1','password2')
+    
+def signup_view(request):
+    if request.method=='POST':
+        form=SignUpForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('login')
+    else:
+        form=SignUpForm()
+    return render(request,'registration/signup.html', {'form': form})
