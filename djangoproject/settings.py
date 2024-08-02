766d734d7e040589e27c7bf8028fb7677de54292
haessar <wahhill@gmail.com>
2024-08-02 11:47:04 +0100

    fix user upload URL and ROOT

--- djangoproject/settings.py
+++ djangoproject/settings.py
@@ -145,5 +145,5 @@ LOGOUT_REDIRECT_URL = "/accounts/login/"
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
 AUTH_USER_MODEL = "IMID.CustomUser"
-MEDIA_URL = "/data/mingcanIMID/"
-MEDIA_ROOT = os.path.join("/data/mingcanIMID/", "uploaded")
+MEDIA_URL = "/media/"
+MEDIA_ROOT = os.path.join(BASE_DIR, "uploaded")
