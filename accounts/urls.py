e4d8bf931923fbf0f6efd04783165fcd1517a13b
Mingcan <mt229a@togpu1.mvls.gla.ac.uk>
2025-01-14 11:24:23 +0000

    add captcha

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,6 +1,13 @@
-from django.urls import path
+from django.urls import path, include
 from .views import signup_view
+from .views import CustomLoginView
 
 urlpatterns=[
     path('signup/',signup_view,name='signup'),
+    path(
+        "login/",
+        CustomLoginView.as_view(redirect_authenticated_user=True),
+        name="login",
+    ),
+    path("captcha/", include("captcha.urls")),
 ]
