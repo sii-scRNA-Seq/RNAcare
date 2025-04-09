c38385d43b6ee656e629fc6021b2003bf2b2bcbc
William Haese-Hill <wahhill@gmail.com>
2025-04-09 21:51:06 +0100

    set STATIC_ROOT

--- djangoproject/settings.py
+++ djangoproject/settings.py
@@ -138,6 +138,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = "static/"
+STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
 
 LOGIN_REDIRECT_URL = "/"
 LOGIN_URL = "/accounts/login/"
